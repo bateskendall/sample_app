ce9258b2fca3f2bb85db0284fb76c080e47325fd
Kendall Bates <bates.kendall@gmail.com>
2018-10-02 02:53:44 +0000

    Fix issue with production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: kendall-twitter-clone.herokuapp.com }
+  host = 'kendall-twitter-clone.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
