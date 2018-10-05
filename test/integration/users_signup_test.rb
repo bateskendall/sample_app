03f94afcf7ffc8d81a17171c1173873454af88b1
Kendall Bates <bates.kendall@gmail.com>
2018-10-05 23:03:49 +0000

    Add user microposts

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,9 +8,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "invalid signup information" do
     get signup_path
-    assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name: "",
+      post users_path, params: { user: { name: "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
